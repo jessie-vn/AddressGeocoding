9e3557c68a0917d53cc3bbf82761dc3620ad158d
Jessie van Nuenen <484943@student.fontys.nl>
2023-02-15 10:25:16 +0100

    Created a proper base for application

--- AddressGeocoding/js/scripts.js
+++ AddressGeocoding/js/scripts.js
@@ -1,12 +1,21 @@
-if("serviceWorker" in navigator){
-    navigator.serviceWorker.register("service-worker.js").then(function(registering){
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("service-worker.js")
+    .then(function (registering) {
       // Registration was successful
-      console.log("Browser: Service Worker registration is successful with the scope",registering.scope);
-    }).catch(function(error){
+      console.log(
+        "Browser: Service Worker registration is successful with the scope",
+        registering.scope
+      );
+    })
+    .catch(function (error) {
       //The registration of the service worker failed
-      console.log("Browser: Service Worker registration failed with the error",error);
+      console.log(
+        "Browser: Service Worker registration failed with the error",
+        error
+      );
     });
-  } else {
-    //The registration of the service worker failed
-    console.log("Browser: I don't support Service Workers :(");
-  }
+} else {
+  //The registration of the service worker failed
+  console.log("Browser: I don't support Service Workers :(");
+}
