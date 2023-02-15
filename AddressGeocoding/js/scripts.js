7e4523adca5d3b2548c5469e9ac86eaad3b90b74
Jessie van Nuenen <484943@student.fontys.nl>
2023-02-15 11:56:47 +0100

    Add basic map with start marker

--- AddressGeocoding/js/scripts.js
+++ AddressGeocoding/js/scripts.js
@@ -19,3 +19,24 @@ if ("serviceWorker" in navigator) {
   //The registration of the service worker failed
   console.log("Browser: I don't support Service Workers :(");
 }
+
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    mapId: "729d3d60d79d34c0",
+    center: { lat: 51.451224988053085, lng: 5.453770513494129 },
+    zoom: 16,
+    disableDefaultUI: true
+  });
+  
+new google.maps.Marker({
+  position: {lat: 51.451224988053085, lng: 5.453770513494129 },
+  map,
+  title: "Start location",
+  icon:{
+    url: "../images/marker.svg",
+    scaledSize: new google.maps.Size(38,41)
+  }
+});
+}
+
+
