f55ac9212b7e3b955e17dcd59621d9944b1e40db
Jessie van Nuenen <484943@student.fontys.nl>
2023-03-01 11:48:57 +0100

    First working version geocoding

--- AddressGeocoding/js/scripts.js
+++ AddressGeocoding/js/scripts.js
@@ -20,23 +20,59 @@ if ("serviceWorker" in navigator) {
   console.log("Browser: I don't support Service Workers :(");
 }
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    mapId: "729d3d60d79d34c0",
-    center: { lat: 51.451224988053085, lng: 5.453770513494129 },
-    zoom: 16,
-    disableDefaultUI: true
-  });
-  
-new google.maps.Marker({
-  position: {lat: 51.451224988053085, lng: 5.453770513494129 }, //TODO: make it use live location if time
-  map,
-  title: "Your location",
-  icon:{
-    url: "../images/marker.svg",
-    scaledSize: new google.maps.Size(38,41)
-  }
+let mapOptions = {
+  center: { lat: 51.451224988053085, lng: 5.453770513494129 }, //TODO: make it use live location if time
+  zoom: 17,
+};
+
+let map = new L.map('map', mapOptions);
+
+let layer = new L.TileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png');
+map.addLayer(layer);
+
+let customMarker = L.icon({
+  iconUrl: "../images/marker.svg",
+  shadowUrl: "../images/markerShadow.svg",
+  iconSize: [40,40],
+  iconAnchor: [20, 35],
+  shadowSize:   [35, 35], 
+  shadowAnchor: [9, 26]
 });
+
+let customLocation = L.icon({
+  iconUrl: "../images/currentlocation.svg",
+  shadowUrl: "../images/currentlocationShadow.svg",
+  iconSize: [30,30],
+  iconAnchor: [15, 15],
+  shadowSize:   [34, 34], 
+  shadowAnchor: [16, 18]
+});
+
+let IconOptions = {
+  title: "Current location",
+  icon: customLocation
 }
 
+let marker = new L.Marker([51.451224988053085,5.453770513494129],IconOptions);
+marker.addTo(map);
 
+let apiKey = "57859398499044f892a16c2163555c8d";
+
+let searchLocationMarker = null;
+
+const addressSearchControl = L.control.addressSearch(apiKey, {
+  position: "topleft",
+  placeholder: "Enter an address here",
+  resultCallback : (address) => {
+    if(!address){
+      return;
+    }
+    if(searchLocationMarker !== null){
+      map.removeLayer(searchLocationMarker);
+    }
+    searchLocationMarker = L.marker([address.lat, address.lon], {icon: customMarker}).addTo(map);
+    map.setView([address.lat, address.lon], 18);
+  }
+});  
+
+  map.addControl (addressSearchControl);
