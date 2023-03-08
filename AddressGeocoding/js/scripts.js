5e2c8d169eb7524f6f83ae9d99cb5f32e3a66dfe
Jessie van Nuenen <484943@student.fontys.nl>
2023-03-08 15:49:00 +0100

    Added real-time location checking

--- AddressGeocoding/js/scripts.js
+++ AddressGeocoding/js/scripts.js
@@ -21,49 +21,66 @@ if ("serviceWorker" in navigator) {
 }
 
 //Asking for permission with the Notification API
-if(typeof Notification!==typeof undefined){ //First check if the API is available in the browser
-	Notification.requestPermission().then(function(result){ 
-		//If accepted, then save subscriberinfo in database
-		if(result==="granted"){
-			console.log("Browser: User accepted receiving notifications, save as subscriber data!");
-			navigator.serviceWorker.ready.then(function(serviceworker){ //When the Service Worker is ready, generate the subscription with our Serice Worker's pushManager and save it to our list
-				const VAPIDPublicKey="BO-PuUpctlaWL5PzSxzxf3FSpzfhIRYT1SU2OEg9PbzDKiOEZpoElPl_wRwJ7XAe5cOZgH8OckzAUdkYHfUj8OI"; // Fill in your VAPID publicKey here
-				const options={applicationServerKey:VAPIDPublicKey,userVisibleOnly:true} //Option userVisibleOnly is neccesary for Chrome
-				serviceworker.pushManager.subscribe(options).then((subscription)=>{
-          //POST the generated subscription to our saving script (this needs to happen server-side, (client-side) JavaScript can't write files or databases)
-					let subscriberFormData=new FormData();
-					subscriberFormData.append("json",JSON.stringify(subscription));
-					fetch("data/saveSubscription.php",{method:"POST",body:subscriberFormData});
-				});
-			});
-		}
-	}).catch((error)=>{
-		console.log(error);
-	});
+if (typeof Notification !== typeof undefined) {
+  //First check if the API is available in the browser
+  Notification.requestPermission()
+    .then(function (result) {
+      //If accepted, then save subscriberinfo in database
+      if (result === "granted") {
+        console.log(
+          "Browser: User accepted receiving notifications, save as subscriber data!"
+        );
+        navigator.serviceWorker.ready.then(function (serviceworker) {
+          //When the Service Worker is ready, generate the subscription with our Serice Worker's pushManager and save it to our list
+          const VAPIDPublicKey =
+            "BO-PuUpctlaWL5PzSxzxf3FSpzfhIRYT1SU2OEg9PbzDKiOEZpoElPl_wRwJ7XAe5cOZgH8OckzAUdkYHfUj8OI"; // Fill in your VAPID publicKey here
+          const options = {
+            applicationServerKey: VAPIDPublicKey,
+            userVisibleOnly: true,
+          }; //Option userVisibleOnly is neccesary for Chrome
+          serviceworker.pushManager.subscribe(options).then((subscription) => {
+            //POST the generated subscription to our saving script (this needs to happen server-side, (client-side) JavaScript can't write files or databases)
+            let subscriberFormData = new FormData();
+            subscriberFormData.append("json", JSON.stringify(subscription));
+            fetch("data/saveSubscription.php", {
+              method: "POST",
+              body: subscriberFormData,
+            });
+          });
+        });
+      }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
- 
 
 let mapOptions = {
   zoom: 17,
 };
 
+let focused = true;
+
 //TODO: set an option for when location is disabled/blocked
 
-function appendLocation(location) {
-  let latlng = L.latLng(location.coords.latitude, location.coords.longitude);
-  map.panTo(latlng);
-  marker.setLatLng(latlng);
-}
-
 if ("geolocation" in navigator) {
-  navigator.geolocation.getCurrentPosition(function (location) {
-    let latlng = L.latLng(location.coords.latitude, location.coords.longitude);
-    mapOptions.center = latlng;
-    map.setView(latlng, mapOptions.zoom);
-    marker.setLatLng(latlng);
-  }, function (error) {
-    console.log(error);
-  });
+  setInterval(() => {
+    navigator.geolocation.getCurrentPosition(
+    function (location) {
+      let latlng = L.latLng(
+        location.coords.latitude,
+        location.coords.longitude
+      );
+      marker.setLatLng(latlng);
+      console.log("Accuracy: " + location.coords.accuracy);
+      if (focused) {
+        map.panTo(latlng);
+      }
+    },
+    function (error) {
+      console.log(error);
+    }
+  ) }, 5000);
 } else {
   target.innerText = "Geolocation API not supported.";
 }
@@ -106,6 +123,47 @@ let MarkerOptions = {
 let marker = new L.Marker([0, 0], IconOptions);
 marker.addTo(map);
 
+const myLocationControl = L.Control.extend({
+  options: {
+    position: "topleft",
+  },
+
+  onAdd: function (map) {
+    const container = L.DomUtil.create(
+      "div",
+      "leaflet-bar leaflet-control leaflet-button"
+    );
+
+    const button = L.DomUtil.create("button", "location-button", container);
+    button.style.width = "30px";
+    button.style.height = "30px";
+    button.style.backgroundImage = 'url("../images/currentlocation.svg")';
+    button.style.backgroundSize = "contain";
+    button.title = "My location";
+
+    L.DomEvent.on(button, "click", function () {
+      focused = true;
+      if ("geolocation" in navigator) {
+        navigator.geolocation.getCurrentPosition(
+          function (location) {
+            let latlng = L.latLng(
+              location.coords.latitude,
+              location.coords.longitude
+            );
+            marker.setLatLng(latlng);
+            map.panTo(latlng);
+          },
+          function (error) {
+            console.log(error);
+          }
+        );
+      }
+    });
+
+    return container;
+  },
+});
+
 let apiKey = "57859398499044f892a16c2163555c8d";
 
 let searchLocationMarker = null;
@@ -115,6 +173,10 @@ const addressSearchControl = L.control.addressSearch(apiKey, {
   placeholder: "Enter an address here",
   resultCallback: (address) => {
     if (!address) {
+      if (searchLocationMarker !== null) {
+        map.removeLayer(searchLocationMarker);
+        MarkerOptions.title = null;
+      }
       return;
     }
     if (searchLocationMarker !== null) {
@@ -126,41 +188,11 @@ const addressSearchControl = L.control.addressSearch(apiKey, {
       [address.lat, address.lon],
       MarkerOptions
     ).addTo(map);
+    focused = false;
     map.setView([address.lat, address.lon], 18);
   },
 });
 
 map.addControl(addressSearchControl);
-
-const myLocationControl = L.Control.extend({
-  options: {
-    position: "topleft"
-  },
-
-  onAdd: function(map) {
-    const container = L.DomUtil.create("div", "leaflet-bar leaflet-control leaflet-button");
-
-    const button = L.DomUtil.create("button", "location-button", container);
-    button.style.width = "30px";
-    button.style.height = "30px";
-    button.style.backgroundImage = 'url("../images/currentlocation.svg")';
-    button.style.backgroundSize = "contain";
-    button.title = "My location";
-
-    L.DomEvent.on(button, "click", function() {
-      if ("geolocation" in navigator) {
-        navigator.geolocation.getCurrentPosition(function(location) {
-          appendLocation(location);
-        }, function(error) {
-          console.log(error);
-        });
-      } else {
-        console.log("Geolocation API not supported.");
-      }
-    });
-
-    return container;
-  }
-});
 
 map.addControl(new myLocationControl());
